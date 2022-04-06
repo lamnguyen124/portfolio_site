217867ea76f13fd0420374d9831d1cd5f00beba9
Lam Nguyen <lam@Lams-MacBook-Pro.local>
2022-04-05 21:05:48 -0500

    accessing projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,19 +3,19 @@ import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
-const projectsData = [{
-        title: 'Project 1',
-        description: 'My first project'
-    },{
-    title: 'Project 2',
-        description: 'My first project'
-},{
-    title: 'Project 3',
-        description: 'My first project'
-},{
-    title: 'Project 4',
-        description: 'My first project'
-}];
+// const projectsData = [{
+//         title: 'Project 1',
+//         description: 'My first project'
+//     },{
+//     title: 'Project 2',
+//         description: 'My first project'
+// },{
+//     title: 'Project 3',
+//         description: 'My first project'
+// },{
+//     title: 'Project 4',
+//         description: 'My first project'
+// }];
 
 
 const Projects = () => (
@@ -23,7 +23,7 @@ const Projects = () => (
     <SectionDivider/>
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-        {projectsData.map((project) => (
+        {projects.map((project) => (
             <div>
                 {project.title}
                 <br/>
